3143d41afd7b7b62650fa0c8a9f22c07ee69f1b4
Gina Vasiloff <gina@schawelcoles.com>
2014-12-01 22:11:25 -0700

    Removed autopublish

--- collections.js
+++ collections.js
@@ -1,3 +1,25 @@
 Tracks = new Mongo.Collection('tracks');
 MyTags = new Mongo.Collection('myTags');
 MyTracks = new Mongo.Collection('myTracks');
+
+if( Meteor.isServer ){
+	
+	Meteor.publish("tracks", function () {
+			return Tracks.find({});
+	});
+	
+	Meteor.publish("myTags", function () {
+			return MyTags.find({});
+	});	
+	
+	Meteor.publish("myTracks", function () {
+			return MyTracks.find({});
+	});	
+	
+}
+
+if( Meteor.isClient ){
+	Meteor.subscribe("tracks");
+	Meteor.subscribe("myTags");
+	Meteor.subscribe("myTracks");
+}
